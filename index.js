3a0865156ca52225ba9d76d78a92fe246c3260de
Nazutte <privacy9136@gmail.com>
2023-01-05 11:33:02 +0500

    my table

--- index.js
+++ index.js
@@ -98,19 +98,30 @@ const table = {
   ['Laxman', '26', 'Male', '70', '90', '75', '90'],
 ]};
 
-const table1 = {
+let table1 = {
   headerRows: 1,
-  widths: [ '*' ],
+  widths: [],
   body: [
-    [{ text: 'Name' }],
+    [],
+    [{ text: 'CASH IN', colSpan: 17, fontSize: 7, bold: true }],
   ],
 };
 
+table1.widths.push(80);
+table1.body[0].push({ text: 'Details', alignment: 'center', fontSize: 6, bold: true, fillColor: '#dedede' });
+for(let count = 0; count < 15; count++){
+  table1.widths.push('*');
+  table1.body[0].push({ text: count + 1, alignment: 'center', fontSize: 6, bold: true, fillColor: '#dedede' });
+}
+table1.widths.push('*');
+table1.body[0].push({ text: 'Total', alignment: 'center', fontSize: 6, bold: true, fillColor: '#dedede' });
+
 let listTableDocs = {
+  pageSize: 'A4',
+  pageOrientation: 'landscape',
   content: [
-    { table1 },
+    { table: table1 },
   ],
-  styles,
 };
 
 const pdfDoc = pdfmake.createPdfKitDocument(listTableDocs, {});
